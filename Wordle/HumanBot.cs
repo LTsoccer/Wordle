674832073d69a1611478b481cc11dbdf4accce01
LTsoccer <77810494+LTsoccer@users.noreply.github.com>
2022-05-04 16:05:49 -0500

    implemented safeguards

--- Wordle/HumanBot.cs
+++ Wordle/HumanBot.cs
@@ -7,15 +7,24 @@ namespace Wordle
 	{
 		public HumanBot()
 		{
-		}
+            Words = new List<string>();
+            foreach (string line in System.IO.File.ReadLines(@"../../../data/english_words_full.txt"))
+            {
+                Words.Add(line);
+            }
+            GuessWords = Words;
+        }
 
-        public List<GuessResult> Guesses { get ; set; } = new List<GuessResult>();
+        private List<string> Words { get; set; }
+
+        private List<string> GuessWords { get; set; }
+        public List<GuessResult> Guesses { get; set; } = new List<GuessResult>();
 
         public string GenerateGuess()
         {
             Console.WriteLine("Enter a 5-letter guess");
             var guess = Console.ReadLine();
-            if (guess.Length != 5)
+            while (guess.Length != 5 || !IsValidWord(guess))
             {
                 Console.WriteLine("Try Again!");
                 Console.WriteLine("Enter a 5-letter guess");
@@ -23,6 +32,14 @@ namespace Wordle
             }
 
             return guess;
+        }
+        public bool IsValidWord(string word)
+        {
+            if (Words.Contains(word))
+            {
+             return true;
+            }
+            return false;
         }
     }
 }
